8edcf9f667aebb2ad248f8c65f67cda4eede53d6
Adam Kruk <adam.kruk@sdworx.com>
2022-05-09 10:27:11 +0200

    Done

---
+++ TicTacToe/TicTacToe/Models/Board.cs
@@ -0,0 +1,35 @@
+﻿namespace TicTacToe.Models
+{
+    public class Board
+    {
+        public string[] chars = new string[9];
+        public string winText = "";
+        public bool player = true;
+        private int[,] winLines = new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
+
+        public bool solve()
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                if (chars[winLines[i, 0]] != null && chars[winLines[i, 1]] != null && chars[winLines[i, 2]] != null)
+                {
+                    if (chars[winLines[i, 0]] == chars[winLines[i, 1]] && chars[winLines[i, 0]] == chars[winLines[i, 2]])
+                    {
+                        winText = chars[winLines[i, 0]] + " wins";
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public void fill()
+        {
+            for (int i = 0; i < 9; ++i)
+            {
+                if (chars[i] == null)
+                    chars[i] = " ";
+            }
+        }
+
+    }
+}
---
+++ TicTacToe/TicTacToe/View/MainView.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TicTacToe.Commands;
+using TicTacToe.Models;
+
+namespace TicTacToe.View
+{
+    public class MainView : INotifyPropertyChanged
+    {
+        private Board board = new Board();
+        private ICommand _fieldCommand;
+        private ICommand _resetGame;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand FieldCommand => _fieldCommand ?? (_fieldCommand = new RelayCommand(FieldAction, FieldExecute));
+
+        public ICommand ResetGame => _resetGame ?? (_resetGame = new RelayCommand(NewGame, ReturnTrue));
+
+        public string[] Chars => board.chars;
+
+        public string WinText => board.winText;
+
+        public string Player => board.player ? "X" : "O";
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void FieldAction(object o)
+        {
+            try
+            {
+                int.TryParse(o.ToString(), out var field);
+                board.chars[field] = board.player ? "X" : "O";
+                board.player = !board.player;
+
+                if (board.solve())
+                {
+                    board.fill();
+                    OnPropertyChanged("WinText");
+                }
+
+                OnPropertyChanged("Chars");
+                OnPropertyChanged("Player");
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private bool FieldExecute(object o)
+        {
+            try
+            {
+                if (o == null) return true;
+                int.TryParse(o.ToString(), out var field);
+                return board.chars[field] == null;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        private void NewGame(object o)
+        {
+            try
+            {
+
+                board.chars = new string[9];
+                board.player = true;
+                board.winText = "";
+                OnPropertyChanged("Chars");
+                OnPropertyChanged("WinText");
+                OnPropertyChanged("Player");
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private bool ReturnTrue(object o)
+        {
+            return true;
+        }
+    }
+}
